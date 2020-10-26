4a639ae0a183620caf02ade3776236ce66462515
Noé <noe.louvel@linkt.fr>
2020-10-26 10:21:32 +0100

    Test

--- src/component/Test.tsx
+++ src/component/Test.tsx
@@ -34,7 +34,7 @@ const TestTheme = () => {
         <Theme>
             <Container>
                 <Heading>Hello World</Heading>
-                <h2>By the power of styled-components!</h2>
+               
             </Container>
 
             <ContainerDanger>
